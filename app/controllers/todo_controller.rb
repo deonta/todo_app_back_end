c3db4992b70835b805c9fbde46815e512599fcf8
Deonta Davenport <deontadavenport72@gmail.com>
2017-10-21 18:40:26 +0000

    fixed back button and added connections to todos

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,23 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 5
+        if params[:id]=="1"
+            @todo_description = "Make the curriculum"
+            @todo_pomodoro_estimate = 5
+        elsif params[:id] =="2"
+            @todo_description = "Play basketball"
+            @todo_pomodoro_estimate = 3
+        elsif params[:id] =="3"
+            @todo_description = "Buy groceries"
+            @todo_pomodoro_estimate = 4
+        elsif params[:id] =="4"
+            @todo_description = "Buy workshop supplies"
+            @todo_pomodoro_estimate = 2
+        elsif params[:id] =="5"
+            @todo_description = "Do my homework"
+            @todo_pomodoro_estimate = 5
+            
+        end
     end
+    
 end
